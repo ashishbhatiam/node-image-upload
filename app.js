67c750ab1834c5e009f9a170ac473fa580eec50f
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-10-08 22:29:44 +0530

    Initial Setup Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+
+// connectDB
+const connectDB = require('./db/connect')
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.get('/', (req, res) => {
+  res.send('<h1>Node Image Upload</h1>')
+})
+
+// middleware
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5001
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+
+    app.listen(port, () => console.log(`Server is listening on PORT: ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
